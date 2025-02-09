796f45a23ca185b13984698c66f175d0d8af5484
Aisling Lee <aislinglee@gmail.com>
2025-02-09 22:19:26 +0000

    Create pi-hole_git_Installer.sh
    Adding the pi-hole repository installer steps

---
+++ Installers/pi-hole_git_Installer.sh
@@ -0,0 +1,7 @@
+# Below are the commands to download and start the install of the pi-hole software
+# Read more at https://pi-hole.net/
+# Source Material: https://github.com/pi-hole/pi-hole/
+
+git clone --depth 1 https://github.com/pi-hole/pi-hole.git Pi-hole
+cd "Pi-hole/automated install/"
+sudo bash basic-install.sh
