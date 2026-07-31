b6c63c92002a05d442d13e9ad434386e60c4eb9a
Aisling Lee <aislinglee@gmail.com>
2026-07-31 19:58:45 +0100

    Update tailscale_install.sh
    adding in details for the IP forwarding

--- system_setup/tailscale_install.sh
+++ system_setup/tailscale_install.sh
@@ -14,3 +14,10 @@ curl -fsSL https://tailscale.com/install.sh | sh
 #Launch it
 # sudo tailscale up # when running on a non DNS server
 sudo tailscale up --accept-dns=false # if runing on pi-hole/device acting as DNS server
+
+#Configure the IP forwarding so as your devices use your pi hole 
+#source - https://tailscale.com/docs/features/subnet-routers/how-to/setup
+echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
+echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
+sudo sysctl -p /etc/sysctl.d/99-tailscale.conf
+sudo tailscale set --advertise-routes=XXX.XXX.XX.0/24,XXX.XXX.XX.0/24 #update to X's to be numbers for the address of the subnets you wish to enable  
